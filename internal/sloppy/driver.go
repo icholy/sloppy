a2781993645e2f52a123c6bab977f2911a55dd48
Ilia Choly <ilia.choly@gmail.com>
2025-05-31 14:54:15 -0400

    update

--- internal/sloppy/driver.go
+++ internal/sloppy/driver.go
@@ -3,6 +3,7 @@ package sloppy
 import (
 	"context"
 
+	"github.com/icholy/sloppy/internal/mcpx"
 	"github.com/mark3labs/mcp-go/mcp"
 )
 
@@ -11,32 +12,34 @@ type AgentV2 interface {
 }
 
 type Driver struct {
-	a AgentV2
+	tools map[string]Tool
+	agent AgentV2
 }
 
 func (d *Driver) Run(ctx context.Context, prompt string) error {
-
-	input := &RunInput{
-		Prompt: prompt,
-	}
-
+	input := &RunInput{Prompt: prompt}
 	for {
-		output, err := d.a.Run(ctx, input)
+		output, err := d.agent.Run(ctx, input)
 		if err != nil {
 			return err
 		}
 		if req := output.CallToolRequest; req != nil {
-			input = &RunInput{
-				CallToolResult: d.call(req),
+			res, err := d.call(ctx, *req)
+			if err != nil {
+				return err
 			}
+			input = &RunInput{CallToolResult: res}
 			continue
 		}
 		break
 	}
-
 	return nil
 }
 
-func (d *Driver) call(req *mcp.CallToolRequest) *mcp.CallToolResult {
-	return nil
+func (d *Driver) call(ctx context.Context, req mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+	tool, ok := d.tools[req.Method]
+	if !ok {
+		return mcpx.NewToolResultErrorf("tool not found: %q", req.Method), nil
+	}
+	return tool.Client.CallTool(ctx, req)
 }
